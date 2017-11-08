4cb1855220ce70aec93d63050afa074c6ec9d928
nekosuke0728 <hatarakazarumono.kuubekarazu00@gmail.com>
2017-11-09 00:36:30 +0900

    Devise(user,staff) MVC追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
   root 'home#top'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',    
+    registrations: 'users/registrations'
+  }
+
+  devise_for :staffs, controllers: {
+    sessions: 'staffs/sessions',
+    passwords: 'staffs/passwords',    
+    registrations: 'staffs/registrations'
+  }
   
 end
