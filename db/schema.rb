f6dd851e17722a5d256c1f7c2e062fea365fcf94
nekosuke0728 <hatarakazarumono.kuubekarazu00@gmail.com>
2017-11-13 02:32:50 +0900

    階層カテゴリ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112145142) do
+ActiveRecord::Schema.define(version: 20171112170716) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 20171112145142) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_id"
+    t.integer "lft"
+    t.integer "rgt"
+    t.index ["lft"], name: "index_categories_on_lft"
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
+    t.index ["rgt"], name: "index_categories_on_rgt"
   end
 
   create_table "esthe_menus", force: :cascade do |t|
